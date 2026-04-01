a1642f9311324347921dfd8a0010d33b3552e6d7
zwallocatememory! <kizley706@gmail.com>
2026-04-01 11:40:48 +0100

    Update visible-check.h

--- visible-check.h
+++ visible-check.h
@@ -1,1 +1,16 @@
 
+// visible check ( credits to offsets.host )
+
+bool is_visible( uintptr_t mesh )
+{
+        // WasRecentlyRendered
+        auto world = yourdriver->read<uintptr_t>( mesh + 0xB0 );
+        auto delta_time_seconds = yourdriver->read<float>( world + 0x88c );
+        auto time_seconds = yourdriver->read<double>( world + 0x868 );
+
+        auto last_render_time = yourdriver->read<float>( mesh + 0x330 );
+        auto time_since = time_seconds - last_render_time;
+
+        auto render_time_threshold = fmaxf( 0.06f , delta_time_seconds + 0.000099999997f );
+        return time_since <= render_time_threshold;
+}
